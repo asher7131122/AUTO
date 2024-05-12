0a32e87200651c0439ac7bae8e380dcb6194df30
Yawa <166945403+Pakyuw@users.noreply.github.com>
2024-05-12 19:18:50 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -17,17 +17,17 @@ module.exports.run = async function({
 }) {
   const input = args.join(' ');
   if (!input) {
-    api.sendMessage(`putangina mo lagyan mo question example: ai 5 little monkey jumping on the bed?'`, event.threadID, event.messageID);
+    api.sendMessage(`i love you baby ${name}\ ano gawa mo baby?'`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`wait boss "${input}"`, event.threadID, event.messageID);
+  api.sendMessage(`wait love "${input}"`, event.threadID, event.messageID);
   try {
     const {
       data
     } = await axios.get(`https://soyeon-gpt4.onrender.com/api?prompt=${encodeURIComponent(input)}`);
     const response = data.response;
-    api.sendMessage(response + '\n\nhttps://bit.ly/create-chatbot-me', event.threadID, event.messageID);
+    api.sendMessage(response + '\n\nhttps://auto-4ltq.onrender.com', event.threadID, event.messageID);
   } catch (error) {
-    api.sendMessage('Yawa ka na mali pre.', event.threadID, event.messageID);
+    api.sendMessage('love di ko hanap ☹️🥺.', event.threadID, event.messageID);
   }
 };
